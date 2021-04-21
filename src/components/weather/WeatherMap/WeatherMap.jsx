0107d2971531d265ce413ade2a6b9bfacd0ba98f
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2021-04-21 17:38:58 +0000

    Creating the Map

--- src/components/weather/WeatherMap/WeatherMap.jsx
+++ src/components/weather/WeatherMap/WeatherMap.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { WeatherMapWrapper } from "./WeatherMap.styles";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import L from "leaflet";
 
 const WeatherMap = () => {
-  return <WeatherMapWrapper>WeatherMap</WeatherMapWrapper>;
+  const icon = L.icon({
+    iconUrl: require("../../../assets/images/pin.png").default,
+    iconSize: [50, 50], // size of the icon
+  });
+  return (
+    <WeatherMapWrapper>
+      <MapContainer
+        style={{ width: "100%", height: "100%" }}
+        center={[33.505, -6.09]}
+        zoom={6}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker icon={icon} position={[33.505, -6.09]}>
+          <Popup>Popop</Popup>
+        </Marker>
+      </MapContainer>
+    </WeatherMapWrapper>
+  );
 };
 
 export default WeatherMap;
--- src/components/weather/WeatherMap/WeatherMap.styles.js
+++ src/components/weather/WeatherMap/WeatherMap.styles.js
@@ -2,5 +2,4 @@ import styled from "styled-components";
 
 export const WeatherMapWrapper = styled.div`
   width: 60%;
-  background: #999;
 `;
